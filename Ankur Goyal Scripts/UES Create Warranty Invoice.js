32de42acfc8dc9fd80713bcad889107fe817052d
Frank Lozano <flozano00@gmail.com>
2019-10-14 08:47:27 -0400

    10/14/2019

---
+++ Ankur Goyal Scripts/UES Create Warranty Invoice.js
@@ -0,0 +1,218 @@
+// BEGIN SCRIPT DESCRIPTION BLOCK  ==================================
+{
+/*
+   	Script Name:
+	Author:
+	Company:
+	Date:
+	Description:
+
+
+	Script Modification Log:
+
+	-- Date --			-- Modified By --				--Requested By--				-- Description --
+
+
+
+Below is a summary of the process controls enforced by this script file.  The control logic is described
+more fully, below, in the appropriate function headers and code blocks.
+
+
+     BEFORE LOAD
+		- beforeLoadRecord(type)
+
+
+
+     BEFORE SUBMIT
+		- beforeSubmitRecord(type)
+
+
+     AFTER SUBMIT
+		- afterSubmitRecord(type)
+
+
+
+     SUB-FUNCTIONS
+		- The following sub-functions are called by the above core functions in order to maintain code
+            modularization:
+
+               - NOT USED
+
+*/
+}
+// END SCRIPT DESCRIPTION BLOCK  ====================================
+
+
+
+// BEGIN GLOBAL VARIABLE BLOCK  =====================================
+{
+	//  Initialize any Global Variables, in particular, debugging variables...
+
+
+
+
+}
+// END GLOBAL VARIABLE BLOCK  =======================================
+
+
+
+
+
+// BEGIN BEFORE LOAD ==================================================
+
+function beforeLoadRecord_setWarrantyField(type, form){
+	/*  On before load:
+	 - EXPLAIN THE PURPOSE OF THIS FUNCTION
+	 -
+	 FIELDS USED:
+	 --Field Name--				--ID--
+	 */
+	//  LOCAL VARIABLES
+
+	//  BEFORE LOAD CODE BODY
+	try {
+
+		if (type == 'create' || type == 'copy') {
+			//var CreatedFrom = nlapiGetFieldValue('createdfrom');
+			//nlapiLogExecution('DEBUG', 'UES Create Invoice', 'createdfrom -->' + CreatedFrom);
+			//if (CreatedFrom == '15618600')
+			{
+				var LineCount = nlapiGetLineItemCount('item')
+				nlapiLogExecution('DEBUG', 'UES Create Invoice', 'LineCount -->' + LineCount);
+
+				for (var i = 1; i <= LineCount; i++) {
+					nlapiSelectLineItem('item', i)
+
+
+					nlapiSetCurrentLineItemValue('item', 'custcol_wrm_reg_warrantyexpire', '');
+					nlapiSetCurrentLineItemValue('item', 'custcol_wrm_reg_warrantyterms', '');
+					nlapiSetCurrentLineItemValue('item', 'custcol_wrm_reg_warrantyreg', '');
+					nlapiSetCurrentLineItemValue('item', 'custcol_wrm_reg_hid_terms', '');
+					nlapiSetCurrentLineItemValue('item', 'custcol_wrm_claimitem', '');
+					nlapiSetCurrentLineItemValue('item', 'custcol_wrm_reg_hid_registerflag', '');
+					nlapiCommitLineItem('item');
+
+				}
+			}
+		}
+	}
+	catch (Execption) {
+		nlapiLogExecution('DEBUG', 'SCH Create Invoice', ' Execption -->' + Execption);
+	}
+	return true;
+}
+
+// END BEFORE LOAD ====================================================
+
+
+
+
+
+// BEGIN BEFORE SUBMIT ================================================
+
+function beforeSubmitRecord_setWarranty(type){
+	/*  On before submit
+	 - PURPOSE
+	 -
+	 FIELDS USED:
+	 --Field Name--				--ID--
+	 */
+	//  LOCAL VARIABLES
+
+
+	//  BEFORE SUBMIT CODE BODY
+
+	if (type == 'create' || type == 'copy') {
+		try {
+			var Customer = nlapiGetFieldValue('entity');
+
+			var CustCategory = nlapiLookupField('customer', Customer, 'category');
+
+			if (CustCategory == '1' || CustCategory == '22') {
+
+				var LineCount = nlapiGetLineItemCount('item')
+
+				nlapiLogExecution('DEBUG', 'UES Create Invoice', 'LineCount -->' + LineCount);
+
+				var InvoiceDate = nlapiGetFieldValue('trandate');
+
+				var DateObj = nlapiStringToDate(InvoiceDate);
+
+				var WarrantyDateObj = nlapiAddDays(DateObj, 30);
+
+				var WarrantyDate = nlapiDateToString(WarrantyDateObj);
+
+				for (var i = 1; i <= LineCount; i++) {
+					var TrackWarranty = nlapiGetLineItemValue('item', 'custcol_wrm_reg_hid_trackwarranty', i);
+					if (TrackWarranty == 'T') {
+						nlapiSelectLineItem('item', i)
+						nlapiSetCurrentLineItemValue('item', 'custcol_wrm_reg_warrantyreg', 'T');
+						nlapiSetCurrentLineItemValue('item', 'custcol_wrm_reg_hid_registerflag', 'T');
+						nlapiSetCurrentLineItemValue('item', 'custcol_wrm_reg_warrantyterms', '4');
+						nlapiSetCurrentLineItemValue('item', 'custcol_wrm_reg_hid_warrantyterms', '4');
+						nlapiSetCurrentLineItemValue('item', 'custcol_wrm_reg_hid_terms', '4');
+						nlapiSetCurrentLineItemValue('item', 'custcol_wrm_reg_warrantyexpire', WarrantyDate);
+						nlapiCommitLineItem('item');
+					}
+				}
+			}
+		}
+		catch (Execption) {
+			nlapiLogExecution('DEBUG', 'SCH Create Invoice', ' Execption -->' + Execption);
+		}
+	}
+	return true;
+}
+
+// END BEFORE SUBMIT ==================================================
+
+
+
+
+
+// BEGIN AFTER SUBMIT =============================================
+
+function afterSubmitRecord_CreateWarrantyInv(type){
+	/*  On after submit:
+	 - PURPOSE
+	 -
+	 FIELDS USED:
+	 --Field Name--				--ID--
+	 */
+	//  LOCAL VARIABLES
+
+
+	//  AFTER SUBMIT CODE BODY
+
+	//try {
+	//	if (type == 'create' || type == 'edit') {
+	//		var i_recordId = nlapiGetRecordId();
+	//		var s_recordtype = nlapiGetRecordType();
+
+	//		var o_recordObj = nlapiLoadRecord(s_recordtype, i_recordId);
+
+	//		var o_INVObj = nlapiTransformRecord(s_recordtype, i_recordId, 'invoice');
+	//		//o_paymentObj.setFieldValue('customform', 70);
+	//		//o_paymentObj.setFieldValue('trandate', TranDate);
+	//		//o_paymentObj.setFieldValue('account', 163);
+	//		//o_paymentObj.setFieldValue('paymentmethod', 12);
+	//		//o_paymentObj.setFieldValue('custbody_auto_create_trans', 'T');
+	//		var InvoiceId = nlapiSubmitRecord(o_INVObj);
+	//		nlapiLogExecution('DEBUG', 'UES Create Warrenty Invoice', 'InvoiceId -->' + InvoiceId);
+
+	//	}
+	//}
+	//catch (ex) {
+	//	nlapiLogExecution('DEBUG', 'Remove Spam Leads', 'ex -->' + ex);
+	//}
+	return true;
+}
+// END AFTER SUBMIT ===============================================
+
+
+
+
+
+
+
+// END FUNCTION =====================================================
